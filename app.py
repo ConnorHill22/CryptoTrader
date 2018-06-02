ff015c49b5ccc8c3d2855f875acdd55666d1050d
Connor Hill <Connor@Connors-MacBook-Pro.local>
2018-06-02 11:45:58 -0400

    a

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from datetime import datetime
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    r = requests.get('http://httpbin.org/status/418')
 
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
+    <p>{r}</p>
 
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+    """.format(time=the_time r=r)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
